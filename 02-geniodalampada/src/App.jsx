10a6dfec795359838de7afccc4ddecd7f6e6063b
Thallis Matos <thallismatos32@gmail.com>
2025-01-24 18:13:00 -0300

    :sparkles: feat: gênio da lâmpada

--- 02-geniodalampada/src/App.jsx
+++ 02-geniodalampada/src/App.jsx
@@ -1,9 +1,32 @@
-function App() {
+import { useState } from "react";
+import lampada from "./assets/Lâmpada.png";
+import genio from "./assets/Gênio.png";
+import "./Style.scss";
+
+export default function App() {
+  const [aladin, setAladin] = useState(lampada);
+
+  const genioDaLampada = () => {
+    setAladin(genio);
+  };
+
+  const voltarLampada = () => {
+    setAladin(lampada);
+  };
+
+  const handleClick = () => {
+    if (aladin === lampada) {
+      genioDaLampada();
+    } else {
+      voltarLampada();
+    }
+  };
+
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <main>
+      <h1>Liberte a magia da programação com a lâmpada de Aladim.</h1>
+      <img src={aladin} alt="Imagem da Lâmpada magica do Aladin" />
+      <button onClick={handleClick} >Clique Aqui</button>
+    </main>
   );
 }
-
-export default App;
